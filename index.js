e706404c000fc78b15c9d2c088939176365d57bb
h3rl <halvard.erla@gmail.com>
2023-01-06 21:22:24 +0100

    beatify

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const path = require("path")
-const prompt = require('prompt-sync')()
+const path = require('path');
+const prompt = require('prompt-sync')();
 
-const config = require(path.resolve('./config.json'))
+const config = require(path.resolve('./config.json'));
 
-const SteamClient = require("./src/steamclient")
+const SteamClient = require('./src/steamclient');
 
 //      Steam
 ////////////////////////
 var client = new SteamClient({
-    username: config.username,
-    password:config.password,
-    games: config.games
-})
+	username: config.username,
+	password: config.password,
+	games: config.games,
+});
 
-client.on("steamguard",(needed)=>{
-    client.authCode = prompt("code: ")
-})
+client.on('steamguard', () => {
+	client.authCode = prompt('code: ');
+	console.log('authenticating, please wait..');
+});
 
-client.on("message",(message)=>{
-    const t = new Date();
-    console.log(t.toLocaleString(),":",message)
-})
+client.on('message', (message) => {
+	const t = new Date();
+	console.log(t.toLocaleString(), ':', message);
+});
 
-client.login()
+client.login();
